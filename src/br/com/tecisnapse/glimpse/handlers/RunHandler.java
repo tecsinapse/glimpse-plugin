c077836986f2f318f80457462c6831d456cea93f
rui.hayashi <rui.hayashi@tecsinapse.com.br>
2011-10-19 15:07:58 -0200

    prints script content to the eclipse console

--- src/br/com/tecisnapse/glimpse/handlers/RunHandler.java
+++ src/br/com/tecisnapse/glimpse/handlers/RunHandler.java
@@ -5,7 +5,16 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
@@ -13,12 +22,27 @@ public class RunHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		final IEditorPart activeEditor = activePage.getActiveEditor();
 		if (activeEditor instanceof ITextEditor) {
 			ITextEditor textEditor = (ITextEditor) activeEditor;
 			IDocumentProvider dp = textEditor.getDocumentProvider();
 			IDocument doc = dp.getDocument(textEditor.getEditorInput());
-			System.out.println(doc.get());
+			
+			ConsolePlugin plugin = ConsolePlugin.getDefault();
+			IConsoleManager conMan = plugin.getConsoleManager();
+			MessageConsole console = new MessageConsole("Glimpse", null);
+			conMan.addConsoles(new IConsole[] {console});
+			MessageConsoleStream out = console.newMessageStream();
+			out.println(doc.get());
+			String id = IConsoleConstants.ID_CONSOLE_VIEW;
+			try {
+				IConsoleView view = (IConsoleView) activePage.showView(id);
+				view.display(console);
+			} catch (PartInitException e) {
+				throw new ExecutionException("Error showing console view", e);
+			}
+			
 		}
 		return null;
 	}
